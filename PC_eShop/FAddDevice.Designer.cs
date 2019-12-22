9f40d74b3ad5079cd6b621f4f55e3f9dc0a9ec94
Евгений Троценко <evenguy@yandex.ru>
2019-12-23 03:56:33 +0600

    Реализован макет окна добавление комплектующих

---
+++ PC_eShop/FAddDevice.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace PC_eShop
+{
+    partial class FAddDevice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboCat = new System.Windows.Forms.ComboBox();
+            this.comboMan = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textCount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textDesc = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Название";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(18, 31);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(259, 20);
+            this.textName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Категория";
+            // 
+            // comboCat
+            // 
+            this.comboCat.FormattingEnabled = true;
+            this.comboCat.Location = new System.Drawing.Point(18, 77);
+            this.comboCat.Name = "comboCat";
+            this.comboCat.Size = new System.Drawing.Size(259, 21);
+            this.comboCat.TabIndex = 3;
+            // 
+            // comboMan
+            // 
+            this.comboMan.FormattingEnabled = true;
+            this.comboMan.Location = new System.Drawing.Point(18, 123);
+            this.comboMan.Name = "comboMan";
+            this.comboMan.Size = new System.Drawing.Size(259, 21);
+            this.comboMan.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Производитель";
+            // 
+            // textCount
+            // 
+            this.textCount.Location = new System.Drawing.Point(18, 172);
+            this.textCount.Name = "textCount";
+            this.textCount.Size = new System.Drawing.Size(117, 20);
+            this.textCount.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Начальное кол-во";
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(18, 223);
+            this.textPrice.Name = "textPrice";
+            this.textPrice.Size = new System.Drawing.Size(117, 20);
+            this.textPrice.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Цена";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(141, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "шт.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(141, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "тг.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Техническое описание";
+            // 
+            // textDesc
+            // 
+            this.textDesc.Location = new System.Drawing.Point(18, 277);
+            this.textDesc.Multiline = true;
+            this.textDesc.Name = "textDesc";
+            this.textDesc.Size = new System.Drawing.Size(259, 110);
+            this.textDesc.TabIndex = 13;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.LightGreen;
+            this.btnAdd.FlatAppearance.BorderSize = 2;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Green;
+            this.btnAdd.Location = new System.Drawing.Point(168, 403);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(109, 33);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // FAddDevice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(298, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textDesc);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboMan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboCat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FAddDevice";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.Text = "Добавление комплектующих";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboCat;
+        private System.Windows.Forms.ComboBox comboMan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textPrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textDesc;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
