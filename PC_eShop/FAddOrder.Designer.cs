7194724021bcd8b22b2824ec071b2af1545c726b
Евгений Троценко <evenguy@yandex.ru>
2019-12-23 03:44:31 +0600

    Изменен заголовок окна доб. заказа

--- PC_eShop/FAddOrder.Designer.cs
+++ PC_eShop/FAddOrder.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textSum = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateOrder = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textFIO = new System.Windows.Forms.TextBox();
             this.textPhone = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.dateOrder = new System.Windows.Forms.DateTimePicker();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textSum = new System.Windows.Forms.TextBox();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.textFIO = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.btnAddToList = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -67,26 +67,53 @@
             this.panel1.Size = new System.Drawing.Size(285, 507);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // btnSave
             // 
-            this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(285, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(592, 507);
-            this.panel2.TabIndex = 1;
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.LightGreen;
+            this.btnSave.FlatAppearance.BorderSize = 2;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.ForeColor = System.Drawing.Color.Green;
+            this.btnSave.Location = new System.Drawing.Point(16, 441);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(252, 54);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Добавить заказ";
+            this.btnSave.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // textSum
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Добавление заказа";
+            this.textSum.Location = new System.Drawing.Point(16, 236);
+            this.textSum.Name = "textSum";
+            this.textSum.ReadOnly = true;
+            this.textSum.Size = new System.Drawing.Size(252, 20);
+            this.textSum.TabIndex = 5;
+            this.textSum.Text = "0 тг.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Сумма заказа";
+            // 
+            // dateOrder
+            // 
+            this.dateOrder.Location = new System.Drawing.Point(16, 187);
+            this.dateOrder.Name = "dateOrder";
+            this.dateOrder.Size = new System.Drawing.Size(252, 20);
+            this.dateOrder.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Дата заказа";
             // 
             // groupBox1
             // 
@@ -100,22 +127,6 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Информация о клиенте";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "ФИО";
-            // 
-            // textFIO
-            // 
-            this.textFIO.Location = new System.Drawing.Point(6, 36);
-            this.textFIO.Name = "textFIO";
-            this.textFIO.Size = new System.Drawing.Size(240, 20);
-            this.textFIO.TabIndex = 1;
             // 
             // textPhone
             // 
@@ -133,63 +144,42 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Телефон";
             // 
-            // label4
+            // textFIO
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Дата заказа";
+            this.textFIO.Location = new System.Drawing.Point(6, 36);
+            this.textFIO.Name = "textFIO";
+            this.textFIO.Size = new System.Drawing.Size(240, 20);
+            this.textFIO.TabIndex = 1;
             // 
-            // dateOrder
+            // label2
             // 
-            this.dateOrder.Location = new System.Drawing.Point(16, 187);
-            this.dateOrder.Name = "dateOrder";
-            this.dateOrder.Size = new System.Drawing.Size(252, 20);
-            this.dateOrder.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ФИО";
             // 
-            // label5
+            // label1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 220);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Сумма заказа";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление заказа";
             // 
-            // textSum
+            // panel2
             // 
-            this.textSum.Location = new System.Drawing.Point(16, 236);
-            this.textSum.Name = "textSum";
-            this.textSum.ReadOnly = true;
-            this.textSum.Size = new System.Drawing.Size(252, 20);
-            this.textSum.TabIndex = 5;
-            this.textSum.Text = "0 тг.";
-            // 
-            // btnSave
-            // 
-            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.LightGreen;
-            this.btnSave.FlatAppearance.BorderSize = 2;
-            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSave.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSave.ForeColor = System.Drawing.Color.Green;
-            this.btnSave.Location = new System.Drawing.Point(16, 441);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(252, 54);
-            this.btnSave.TabIndex = 6;
-            this.btnSave.Text = "Добавить заказ";
-            this.btnSave.UseVisualStyleBackColor = false;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.Controls.Add(this.btnAddToList);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 451);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(592, 56);
-            this.panel3.TabIndex = 0;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(285, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(592, 507);
+            this.panel2.TabIndex = 1;
             // 
             // dataGridView1
             // 
@@ -203,6 +193,16 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(592, 451);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.btnAddToList);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 451);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(592, 56);
+            this.panel3.TabIndex = 0;
             // 
             // btnAddToList
             // 
@@ -226,14 +226,14 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "FAddOrder";
-            this.Text = "FAddOrder";
+            this.Text = "Добавление заказа";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
