729036814fa6b2925ba9f2be03589927a092f86f
Евгений Троценко <evenguy@yandex.ru>
2019-12-25 13:03:00 +0600

    Реализовано формирование графика финансов

--- PC_eShop/FReportOrders.Designer.cs
+++ PC_eShop/FReportOrders.Designer.cs
@@ -29,21 +29,20 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboYear = new System.Windows.Forms.ComboBox();
+            this.textYear = new System.Windows.Forms.TextBox();
             this.btnUpdate = new System.Windows.Forms.Button();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chartCash = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCash)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.textYear);
             this.panel1.Controls.Add(this.btnUpdate);
-            this.panel1.Controls.Add(this.comboYear);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -52,25 +51,14 @@
             this.panel1.Size = new System.Drawing.Size(800, 47);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // textYear
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Год";
-            // 
-            // comboYear
-            // 
-            this.comboYear.FormattingEnabled = true;
-            this.comboYear.Items.AddRange(new object[] {
-            "2019",
-            "2020"});
-            this.comboYear.Location = new System.Drawing.Point(49, 12);
-            this.comboYear.Name = "comboYear";
-            this.comboYear.Size = new System.Drawing.Size(121, 21);
-            this.comboYear.TabIndex = 1;
+            this.textYear.Location = new System.Drawing.Point(49, 12);
+            this.textYear.Name = "textYear";
+            this.textYear.Size = new System.Drawing.Size(121, 20);
+            this.textYear.TabIndex = 3;
+            this.textYear.Text = "2019";
+            this.textYear.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnUpdate
             // 
@@ -84,23 +72,32 @@
             this.btnUpdate.TabIndex = 2;
             this.btnUpdate.Text = "Обновить график";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
-            // chart1
+            // label1
             // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Год";
+            // 
+            // chartCash
+            // 
+            chartArea1.AxisX.Maximum = 12D;
+            chartArea1.AxisX.Minimum = 1D;
+            chartArea1.BorderColor = System.Drawing.Color.Gainsboro;
             chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(0, 49);
-            this.chart1.Name = "chart1";
+            this.chartCash.ChartAreas.Add(chartArea1);
+            this.chartCash.Location = new System.Drawing.Point(0, 49);
+            this.chartCash.Name = "chartCash";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
             series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(800, 404);
-            this.chart1.TabIndex = 1;
-            this.chart1.Text = "chart1";
+            this.chartCash.Series.Add(series1);
+            this.chartCash.Size = new System.Drawing.Size(800, 404);
+            this.chartCash.TabIndex = 1;
             // 
             // FReportOrders
             // 
@@ -108,7 +105,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.chartCash);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -118,7 +115,7 @@
             this.Text = "Динамика продаж по месяцам";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCash)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -127,8 +124,8 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnUpdate;
-        private System.Windows.Forms.ComboBox comboYear;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCash;
+        private System.Windows.Forms.TextBox textYear;
     }
 }
--- PC_eShop/FReportOrders.cs
+++ PC_eShop/FReportOrders.cs
@@ -17,9 +17,33 @@ namespace PC_eShop
         public FReportOrders()
         {
             InitializeComponent();
+            updChart();
+        }
 
-            if (comboYear.Items.Count > 0)
-                comboYear.SelectedIndex = 0;
+        // Обновление графика
+        private void updChart()
+        {
+            // Получение данных
+            int year;
+            if ( !Int32.TryParse(textYear.Text, out year) || ( textYear.Text.Length != 4 ) )
+            {
+                MessageBox.Show("Неправильно введен год.\nПример: 2020", "Неправильно введены данные", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<ReportOrders> reports = ReportOrders.getList(year);
+
+            // Заполнение графика
+            chartCash.Series[0].Points.Clear();
+
+            for (int i = 0; i < reports.Count; i++)
+                chartCash.Series[0].Points.AddXY(reports[i].monthNum, reports[i].TotalCash);
+        }
+
+        // Нажата кнопка "Обновить график"
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            updChart();
         }
     }
 
@@ -45,7 +69,12 @@ namespace PC_eShop
                 reader = DataBase.readData("SELECT SUM(TotalPrice) AS 'Sum' FROM [Order] WHERE [OrdDate] BETWEEN '01-"+i.ToString()+"-"+year.ToString()+"' AND '31-"+i.ToString()+ "-" + year.ToString() + "'");
 
                 if ( reader.HasRows && reader.Read() )
-                    list.Add( new ReportOrders(i, reader.GetInt32(0)) );
+                {
+                    if (reader.IsDBNull(0))
+                        list.Add(new ReportOrders(i, 0));
+                    else
+                        list.Add(new ReportOrders(i, reader.GetInt32(0)));
+                }
 
                 reader.Close();
             }
